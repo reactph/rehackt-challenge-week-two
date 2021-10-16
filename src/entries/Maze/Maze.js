60f4e5c2d4326318707f1c22b5464aa013524a05
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-10-17 03:15:43 +0800

    Fix erroneous import

--- src/entries/Maze/Maze.js
+++ src/entries/Maze/Maze.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import { useEffect } from "react/cjs/react.development"
+import React, { useState, useEffect } from "react"
 
 import BaseEntry from "../../components/BaseEntry/BaseEntry"
 import Game from "./Game"
