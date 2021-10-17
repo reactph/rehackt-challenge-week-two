52bf442a645dce5209038ee81a0e400ee0b8c0e0
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-10-17 21:24:42 +0800

    Update index.js

--- src/entries/index.js
+++ src/entries/index.js
@@ -35,7 +35,7 @@ const entries = [
   {
     title: "Just A Maze ;)",
     slug: "JustAMaze",
-    description: "Drag the red box carefully to do red area to win. Be careful of going out of bounds or else you'll go back again.",
+    description: "Drag the red box carefully to the red area to win. Be careful of going out of bounds or else you'll go back again.",
     author: "Franrey Saycon",
     email: "franreysaycon@gmail.com",
     linkedIn: "https://www.linkedin.com/in/fssaycon/",
